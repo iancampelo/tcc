89a5c8ba4d391347a479d4e70ac20c3cbd98ea56
iancampelo <ian.campelo@live.com>
2014-09-10 11:14:43 -0300

    Exclude general settings

--- AssistenteReflexivo/app/src/main/java/assistente/br/ucb/tcc/assistentereflexivo/SettingsActivity.java
+++ AssistenteReflexivo/app/src/main/java/assistente/br/ucb/tcc/assistentereflexivo/SettingsActivity.java
@@ -181,8 +181,6 @@ public class SettingsActivity extends PreferenceActivity {
         // Bind the summaries of EditText/List/Dialog/Ringtone preferences to
         // their values. When their values change, their summaries are updated
         // to reflect the new value, per the Android Design guidelines.
-        bindPreferenceSummaryToValue(findPreference("example_text"));
-        bindPreferenceSummaryToValue(findPreference("example_list"));
         bindPreferenceSummaryToValue(findPreference("sync_frequency"));
     }
 
@@ -211,13 +209,6 @@ public class SettingsActivity extends PreferenceActivity {
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.pref_general);
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-            bindPreferenceSummaryToValue(findPreference("example_text"));
-            bindPreferenceSummaryToValue(findPreference("example_list"));
         }
     }
 
