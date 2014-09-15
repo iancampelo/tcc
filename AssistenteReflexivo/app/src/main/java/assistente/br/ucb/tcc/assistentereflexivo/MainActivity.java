200421c1bc07d20dcdc200fd9ad4f2b680563e8e
iancampelo <ian.campelo@live.com>
2014-09-15 10:26:29 -0300

    TextView txtMain

--- AssistenteReflexivo/app/src/main/java/assistente/br/ucb/tcc/assistentereflexivo/MainActivity.java
+++ AssistenteReflexivo/app/src/main/java/assistente/br/ucb/tcc/assistentereflexivo/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends Activity {
