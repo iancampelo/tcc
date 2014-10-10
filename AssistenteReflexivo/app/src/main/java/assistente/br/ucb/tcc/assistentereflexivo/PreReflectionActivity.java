c942f963aee49469b8ae008bc8cc4b0e1704bb05
iancampelo <ian.campelo@live.com>
2014-10-10 01:00:54 -0300

    Fix imports

--- AssistenteReflexivo/app/src/main/java/assistente/br/ucb/tcc/assistentereflexivo/PreReflectionActivity.java
+++ AssistenteReflexivo/app/src/main/java/assistente/br/ucb/tcc/assistentereflexivo/PreReflectionActivity.java
@@ -8,7 +8,6 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Spinner;
